65a1d215b0ba65f546079be69c26388b0dad4243
HebePython <50890910+HebePython@users.noreply.github.com>
2024-09-24 23:25:41 +0200

    added odometer, distance traveled.

---
+++ Java_Projects/CarDashboard/CardashboardMain/Odometer.java
@@ -0,0 +1,15 @@
+package CarDashboard.CardashboardMain;
+
+public class Odometer {
+    private double kmTraveled;
+    
+    Odometer(double disTraveled) {
+        this.kmTraveled = disTraveled;
+    }
+    public double getKmTraveled() {
+        return kmTraveled;
+    }
+    public void setKmTraveled(double kmTraveled) {
+        this.kmTraveled = kmTraveled;
+    }
+}
