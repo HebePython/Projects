15ca19ec4be6860b1b860ad645b818d844feb8e0
HebePython <50890910+HebePython@users.noreply.github.com>
2024-09-24 23:31:58 +0200

    messing around in cardashboard.

--- Java_Projects/CarDashboard/CardashboardMain/CarDashboard.java
+++ Java_Projects/CarDashboard/CardashboardMain/CarDashboard.java
@@ -1,9 +1,17 @@
 package CarDashboard.CardashboardMain;
 
 public class CarDashboard {
+    private Fuelgauge fuel;         //litres
+    private Odometer kmTraveled;    // distance traveled in km
+    private Speedometer currentSpeed;      // km/h
     
+    CarDashboard(double fuel, double kmTraveled, double speed) {
+  //      this.fuel = fuel;
+    }
+
     public static void main(String[] args) {
         
+   //     CarDashboard temp = new CarDashboard();
         
 
     }
