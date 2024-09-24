3a594c37f883087af2612a504b8a54aae1ae3f29
HebePython <50890910+HebePython@users.noreply.github.com>
2024-09-24 23:13:28 +0200

    added speedometer class file. added main file. (CarDashboard)

---
+++ Java_Projects/CarDashboard/Speedometer.java
@@ -0,0 +1,17 @@
+package CarDashboard;
+
+public class Speedometer {
+    private double kmh;
+
+    Speedometer(double kmh) {
+        this.kmh = kmh;
+    }
+
+    public double getKmh() {
+        return kmh;
+    }
+    public void setKmh(double kmh) {
+        this.kmh = kmh;
+    }
+    
+}
